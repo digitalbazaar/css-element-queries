18e64edff92e04e9000dc809b12708be2616a671
Dave Longley <dlongley@digitalbazaar.com>
2017-01-18 14:47:42 -0500

    Add bower and amd support.

---
+++ main.js
@@ -0,0 +1,13 @@
+define([
+  './src/ResizeSensor',
+  './src/ElementQueries'
+], function(ResizeSensor, ElementQueries) {
+
+'use strict';
+
+return {
+  ResizeSensor: ResizeSensor,
+  ElementQueries: ElementQueries
+};
+
+});
